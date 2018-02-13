c6a2acdda61682683c05f79fe6ffdd63f2ae8683
UCsiriphon <siriphonnot@ucconnect.co.th>
2018-02-13 21:12:53 +0700

    fix banner link

--- resources/views/site/layout.blade.php
+++ resources/views/site/layout.blade.php
@@ -10,8 +10,8 @@
     <meta property="og:type" content="website">
     <meta property="og:site_name" content="Vali Admin">
     <meta property="og:title" content="Vali - Free Bootstrap 4 admin theme">
-    <meta property="og:url" content="http://pratikborsadiya.in/blog/vali-admin">
-    <meta property="og:image" content="http://pratikborsadiya.in/blog/vali-admin/hero-social.png">
+    <meta property="og:url" content="https://laravel.nottdev.com/">
+    <meta property="og:image" content="{{URL::asset('img/banner.png')}}">
     <meta property="og:description" content="Vali is a responsive and free admin theme built with Bootstrap 4, SASS and PUG.js. It's fully customizable and modular.">
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
